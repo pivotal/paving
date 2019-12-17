47924bd11a347f89c8e02e67641bac00ec9cecfd
Evan Farrar <efarrar@pivotal.io>
2019-12-17 12:13:56 -0800

    Not creating an rds instance, so deleting the rds subnet.
    Co-authored-by: Evan Farrar <efarrar@pivotal.io>
    Co-authored-by: dashaun <dacarter@pivotal.io>
    Co-authored-by: Genevieve <glesperance@pivotal.io>

--- aws/subnets.tf
+++ aws/subnets.tf
@@ -3,7 +3,6 @@ locals {
   management_subnet_cidrs = ["10.0.16.0/28", "10.0.16.16/28"]
   pas_subnet_cidrs        = ["10.0.4.0/24", "10.0.5.0/24"]
   services_subnet_cidrs   = ["10.0.8.0/24", "10.0.9.0/24"]
-  rds_subnet_cidrs        = ["10.0.12.0/24", "10.0.13.0/24"]
   pks_subnet_cidrs        = ["10.0.14.0/24", "10.0.15.0/24"]
 }
 
@@ -52,18 +51,6 @@ resource "aws_subnet" "services-subnet" {
 
   tags = {
     Name = "${var.environment_name}-services-subnet-${count.index}"
-  }
-}
-
-resource "aws_subnet" "rds-subnet" {
-  count = length(local.rds_subnet_cidrs)
-
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = element(local.rds_subnet_cidrs, count.index)
-  availability_zone = element(var.availability_zones, count.index)
-
-  tags = {
-    Name = "${var.environment_name}-rds-subnet-${count.index}"
   }
 }
 
