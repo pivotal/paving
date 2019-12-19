a6ae60347fef5365d18c32d29f1a7be726f857ce
Genevieve Lesperance <glesperance@pivotal.io>
2019-12-19 11:57:16 -0500

    Add pas buckets for gcp.

--- gcp/buckets.tf
+++ gcp/buckets.tf
@@ -2,3 +2,29 @@ resource "google_storage_bucket" "ops-manager" {
   name          = "${var.project}-${var.environment_name}-ops-manager"
   force_destroy = true
 }
+
+resource "google_storage_bucket" "buildpacks" {
+  name          = "${var.project}-${var.environment_name}-buildpacks"
+  force_destroy = true
+}
+
+resource "google_storage_bucket" "droplets" {
+  name          = "${var.project}-${var.environment_name}-droplets"
+  force_destroy = true
+}
+
+resource "google_storage_bucket" "packages" {
+  name          = "${var.project}-${var.environment_name}-packages"
+  force_destroy = true
+}
+
+resource "google_storage_bucket" "resources" {
+  name          = "${var.project}-${var.environment_name}-resources"
+  force_destroy = true
+}
+
+resource "google_storage_bucket" "backup" {
+  name          = "${var.project}-${var.environment_name}-backup"
+  force_destroy = true
+}
+
