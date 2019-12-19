0364a012de6ef052b4ed971767b2eedd22b576ff
Genevieve Lesperance <glesperance@pivotal.io>
2019-12-19 12:02:38 -0500

    Move OM firewall to OM template.

--- gcp/ops-manager.tf
+++ gcp/ops-manager.tf
@@ -52,3 +52,18 @@ resource "google_project_iam_member" "storage-admin" {
   member  = "serviceAccount:${google_service_account.ops-manager.email}"
 }
 
+# Allow HTTP/S access to Ops Manager from the outside world
+resource "google_compute_firewall" "ops-manager" {
+  name        = "${var.environment_name}-ops-manager"
+  network     = google_compute_network.network.name
+  target_tags = ["${var.environment_name}-ops-manager"]
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+}
