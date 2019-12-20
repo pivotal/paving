3c357837f1b2118be68e6e040c10d8f428295566
Genevieve Lesperance <glesperance@pivotal.io>
2019-12-20 08:44:53 -0500

    Add gcp tcp lb resources.

--- gcp/lbs.tf
+++ gcp/lbs.tf
@@ -3,6 +3,61 @@ resource "google_compute_address" "tcp-lb" {
   name = "${var.environment_name}-tcp-lb"
 }
 
+locals {
+  tcp_ports = ["1024-1123"]
+}
+
+resource "google_compute_forwarding_rule" "tcp-lb" {
+  name        = "${var.environment_name}-tcp-lb-${count.index}"
+  ip_address  = google_compute_address.tcp-lb.address
+  target      = google_compute_target_pool.tcp-lb.self_link
+  port_range  = element(local.tcp_ports, count.index)
+  ip_protocol = "TCP"
+
+  count = length(local.tcp_ports)
+}
+
+resource "google_compute_target_pool" "tcp-lb" {
+  name = "${var.environment_name}-tcp-lb"
+
+  health_checks = [google_compute_http_health_check.tcp-lb.name]
+}
+
+resource "google_compute_http_health_check" "tcp-lb" {
+  name                = "${var.environment_name}-tcp-lb-health-check"
+  port                = 80
+  request_path        = "/health"
+  check_interval_sec  = 30
+  timeout_sec         = 5
+  healthy_threshold   = 10
+  unhealthy_threshold = 2
+}
+
+resource "google_compute_firewall" "tcp-lb-health-check" {
+  name    = "${var.environment_name}-tcp-lb-health-check"
+  network = google_compute_network.network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+
+  target_tags = ["${var.environment_name}-tcp-lb"]
+}
+
+resource "google_compute_firewall" "tcp-lb" {
+  name    = "${var.environment_name}-tcp-lb-firewall"
+  network = google_compute_network.network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1024-65535"]
+  }
+
+  target_tags = ["${var.environment_name}-tcp-lb"]
+}
 
 # WebSocket Load Balancer
 resource "google_compute_address" "ws-lb" {
