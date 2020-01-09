e73bb4aa3505580cf6c1da0ad4faf0ddd924ff97
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-09 13:16:26 -0500

    Add aws security group ids to output. Need ids and name for config apparently.

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -11,7 +11,6 @@ locals {
     dns_tcp           = aws_route53_record.tcp.name
     dns_pks_api       = aws_route53_record.pks-api.name
 
-    ops_manager_security_group            = aws_security_group.ops-manager.id
     ops_manager_public_ip                 = aws_eip.ops-manager.public_ip
     ops_manager_dns                       = aws_route53_record.ops-manager.name
     ops_manager_iam_user_access_key       = aws_iam_access_key.ops-manager.id
@@ -24,14 +23,24 @@ locals {
 
     region = var.region
 
-    security_group_platform     = aws_security_group.platform.name
-    security_group_nat          = aws_security_group.nat.name
-    security_group_ssh_lb       = aws_security_group.ssh-lb.name
-    security_group_tcp_lb       = aws_security_group.tcp-lb.name
-    security_group_web_lb       = aws_security_group.web-lb.name
-    security_group_mysql        = aws_security_group.mysql.name
-    security_group_pks_internal = aws_security_group.pks-internal-sg.name
-    security_group_pks_api_lb   = aws_security_group.pks-api-lb.name
+    security_group_ops_manager_id    = aws_security_group.ops-manager.id
+    security_group_ops_manager_name  = aws_security_group.ops-manager.name
+    security_group_platform_id       = aws_security_group.platform.id
+    security_group_platform_name     = aws_security_group.platform.name
+    security_group_nat_id            = aws_security_group.nat.id
+    security_group_nat_name          = aws_security_group.nat.name
+    security_group_ssh_lb_id         = aws_security_group.ssh-lb.id
+    security_group_ssh_lb_name       = aws_security_group.ssh-lb.name
+    security_group_tcp_lb_id         = aws_security_group.tcp-lb.id
+    security_group_tcp_lb_name       = aws_security_group.tcp-lb.name
+    security_group_web_lb_id         = aws_security_group.web-lb.id
+    security_group_web_lb_name       = aws_security_group.web-lb.name
+    security_group_mysql_id          = aws_security_group.mysql.id
+    security_group_mysql_name        = aws_security_group.mysql.name
+    security_group_pks_internal_id   = aws_security_group.pks-internal-sg.id
+    security_group_pks_internal_name = aws_security_group.pks-internal-sg.name
+    security_group_pks_api_lb_id     = aws_security_group.pks-api-lb.id
+    security_group_pks_api_lb_name   = aws_security_group.pks-api-lb.name
 
     subnet_public_ids       = aws_subnet.public-subnet[*].id
     subnet_public_cidrs     = aws_subnet.public-subnet[*].cidr_block
