cff1e7a18acade0f0c7046769945aa58406cd6d1
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-09 13:28:41 -0500

    Use tags to ensure env name on azure resources.

--- azure/storage.tf
+++ azure/storage.tf
@@ -1,31 +1,35 @@
 resource "random_string" "ops-manager" {
-  length  = 10
+  length  = 20
   special = false
   upper   = false
 }
 
 resource "azurerm_storage_account" "ops-manager" {
-  name                     = "${var.environment_name}-${random_string.ops-manager.result}"
+  name                     = random_string.ops-manager.result
   resource_group_name      = azurerm_resource_group.platform.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  tags = {
+    environment = var.environment_name
+  }
 }
 
-resource "random_string" "pas_storage_account_name" {
+resource "random_string" "pas" {
   length  = 20
   special = false
   upper   = false
 }
 
 resource "azurerm_storage_account" "pas" {
-  name                     = random_string.pas_storage_account_name.result
+  name                     = random_string.pas.result
   resource_group_name      = azurerm_resource_group.platform.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  tags {
+  tags = {
     environment = var.environment_name
   }
 }
