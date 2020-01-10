2a434f4f97d1adbea3b3772d2b6518e898096035
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-10 12:11:38 -0500

    Update azure variables to clarify hosted zone arg.

--- azure/variables.tf
+++ azure/variables.tf
@@ -1,14 +1,34 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  type = string
+}
 
-variable "environment_name" {}
-variable "location" {}
+variable "client_id" {
+  type = string
+}
 
-variable "hosted_zone" {}
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "environment_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "hosted_zone" {
+  description = "Hosted zone name (e.g. foo.example.com)"
+  type = string
+}
 
 variable "cloud_name" {
   description = "The Azure cloud environment to use. Available values at https://www.terraform.io/docs/providers/azurerm/#environment"
   default     = "public"
+  type = string
 }
