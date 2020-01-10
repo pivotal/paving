448309ffd3916404e7a30ae75dd213e345851d30
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-10 14:06:16 -0500

    Add nsxt lb pool name outputs.

--- nsxt/lbs.tf
+++ nsxt/lbs.tf
@@ -1,4 +1,4 @@
-resource "nsxt_lb_http_monitor" "lb_web_monitor" {
+resource "nsxt_lb_http_monitor" "pas-web" {
   description           = "The Active Health Monitor (healthcheck) for Web (HTTP(S)) traffic."
   display_name          = "${var.environment_name}-pas-web-monitor"
   monitor_port          = 8080
@@ -13,7 +13,7 @@ resource "nsxt_lb_http_monitor" "lb_web_monitor" {
   }
 }
 
-resource "nsxt_lb_http_monitor" "lb_tcp_monitor" {
+resource "nsxt_lb_http_monitor" "pas-tcp" {
   description     = "The Active Health Monitor (healthcheck) for TCP traffic."
   display_name    = "${var.environment_name}-pas-tcp-monitor"
   monitor_port    = 80
@@ -28,7 +28,7 @@ resource "nsxt_lb_http_monitor" "lb_tcp_monitor" {
   response_status_codes = [200]
 }
 
-resource "nsxt_lb_tcp_monitor" "lb_ssh_monitor" {
+resource "nsxt_lb_tcp_monitor" "pas-ssh" {
   description  = "The Active Health Monitor (healthcheck) for SSH traffic."
   display_name = "${var.environment_name}-pas-ssh-monitor"
   monitor_port = 2222
@@ -39,12 +39,12 @@ resource "nsxt_lb_tcp_monitor" "lb_ssh_monitor" {
   }
 }
 
-resource "nsxt_lb_pool" "lb_web_pool" {
+resource "nsxt_lb_pool" "pas-web" {
   description              = "The Server Pool of Web (HTTP(S)) traffic handling VMs"
   display_name             = "${var.environment_name}-pas-web-pool"
   algorithm                = "ROUND_ROBIN"
   tcp_multiplexing_enabled = false
-  active_monitor_id        = nsxt_lb_http_monitor.lb_web_monitor.id
+  active_monitor_id        = nsxt_lb_http_monitor.pas-web.id
 
   snat_translation {
     type = "SNAT_AUTO_MAP"
@@ -56,12 +56,12 @@ resource "nsxt_lb_pool" "lb_web_pool" {
   }
 }
 
-resource "nsxt_lb_pool" "lb_tcp_pool" {
+resource "nsxt_lb_pool" "pas-tcp" {
   description              = "The Server Pool of TCP traffic handling VMs"
   display_name             = "${var.environment_name}-pas-tcp-pool"
   algorithm                = "ROUND_ROBIN"
   tcp_multiplexing_enabled = false
-  active_monitor_id        = nsxt_lb_http_monitor.lb_tcp_monitor.id
+  active_monitor_id        = nsxt_lb_http_monitor.pas-tcp.id
 
   snat_translation {
     type = "TRANSPARENT"
@@ -73,12 +73,12 @@ resource "nsxt_lb_pool" "lb_tcp_pool" {
   }
 }
 
-resource "nsxt_lb_pool" "lb_ssh_pool" {
+resource "nsxt_lb_pool" "pas-ssh" {
   description              = "The Server Pool of SSH traffic handling VMs"
   display_name             = "${var.environment_name}-pas-ssh-pool"
   algorithm                = "ROUND_ROBIN"
   tcp_multiplexing_enabled = false
-  active_monitor_id        = nsxt_lb_tcp_monitor.lb_ssh_monitor.id
+  active_monitor_id        = nsxt_lb_tcp_monitor.pas-ssh.id
 
   snat_translation {
     type = "TRANSPARENT"
@@ -107,7 +107,7 @@ resource "nsxt_lb_tcp_virtual_server" "lb_web_virtual_server" {
   application_profile_id = nsxt_lb_fast_tcp_application_profile.pas_lb_tcp_application_profile.id
   ip_address             = var.nsxt_lb_web_virtual_server_ip_address
   ports                  = ["80", "443"]
-  pool_id                = nsxt_lb_pool.lb_web_pool.id
+  pool_id                = nsxt_lb_pool.pas-web.id
 
   tag {
     scope = "terraform"
@@ -121,7 +121,7 @@ resource "nsxt_lb_tcp_virtual_server" "lb_tcp_virtual_server" {
   application_profile_id = nsxt_lb_fast_tcp_application_profile.pas_lb_tcp_application_profile.id
   ip_address             = var.nsxt_lb_tcp_virtual_server_ip_address
   ports                  = var.nsxt_lb_tcp_virtual_server_ports
-  pool_id                = nsxt_lb_pool.lb_tcp_pool.id
+  pool_id                = nsxt_lb_pool.pas-tcp.id
 
   tag {
     scope = "terraform"
@@ -135,7 +135,7 @@ resource "nsxt_lb_tcp_virtual_server" "lb_ssh_virtual_server" {
   application_profile_id = nsxt_lb_fast_tcp_application_profile.pas_lb_tcp_application_profile.id
   ip_address             = var.nsxt_lb_ssh_virtual_server_ip_address
   ports                  = ["2222"]
-  pool_id                = nsxt_lb_pool.lb_ssh_pool.id
+  pool_id                = nsxt_lb_pool.pas-ssh.id
 
   tag {
     scope = "terraform"
--- nsxt/outputs.tf
+++ nsxt/outputs.tf
@@ -1,6 +1,10 @@
 locals {
   stable_config = {
     nsxt_host = var.nsxt_host
+
+    lb_pool_web = nsxt_lb_pool.pas-web.display_name
+    lb_pool_tcp = nsxt_lb_pool.pas-tcp.display_name
+    lb_pool_ssh = nsxt_lb_pool.pas-ssh.display_name
   }
 }
 
