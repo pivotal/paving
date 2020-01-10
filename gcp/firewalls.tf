af5db176ab839acb7b2f9767e4f1c3f8b5959692
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-10 12:07:06 -0500

    All gcp firewalls together and all iam together.

--- gcp/firewalls.tf
+++ gcp/firewalls.tf
@@ -1,3 +1,21 @@
+resource "google_compute_firewall" "ops-manager" {
+  name    = "${var.environment_name}-ops-manager"
+  network = google_compute_network.network.name
+
+  direction = "INGRESS"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+
+  target_tags = ["${var.environment_name}-ops-manager"]
+}
+
 resource "google_compute_firewall" "internal" {
   name    = "${var.environment_name}-internal"
   network = google_compute_network.network.self_link
