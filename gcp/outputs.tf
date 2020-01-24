8e79f9d109dd077fda1d0ff583080388a010c496
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-23 20:21:23 -0500

    Reorganize gcp outputs.

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -6,26 +6,11 @@ locals {
 
     network_name = google_compute_network.network.name
 
-    dns_name_servers = data.google_dns_managed_zone.hosted-zone.name_servers
+    hosted_zone_name_servers = data.google_dns_managed_zone.hosted-zone.name_servers
 
-    subnet_management_name    = google_compute_subnetwork.management.name
-    subnet_management_cidr    = google_compute_subnetwork.management.ip_cidr_range
-    subnet_management_gateway = google_compute_subnetwork.management.gateway_address
-
-    subnet_pas_name    = google_compute_subnetwork.pas.name
-    subnet_pas_cidr    = google_compute_subnetwork.pas.ip_cidr_range
-    subnet_pas_gateway = google_compute_subnetwork.pas.gateway_address
-
-    subnet_services_name    = google_compute_subnetwork.services.name
-    subnet_services_cidr    = google_compute_subnetwork.services.ip_cidr_range
-    subnet_services_gateway = google_compute_subnetwork.services.gateway_address
-
-    subnet_pks_name    = google_compute_subnetwork.pks.name
-    subnet_pks_cidr    = google_compute_subnetwork.pks.ip_cidr_range
-    subnet_pks_gateway = google_compute_subnetwork.pks.gateway_address
-
-    service_account_pks_master_id = google_service_account.pks-master-node-service-account.email
-    service_account_pks_worker_id = google_service_account.pks-worker-node-service-account.email
+    management_subnet_name    = google_compute_subnetwork.management.name
+    management_subnet_cidr    = google_compute_subnetwork.management.ip_cidr_range
+    management_subnet_gateway = google_compute_subnetwork.management.gateway_address
 
     ops_manager_service_account_key = google_service_account_key.ops-manager.private_key
     ops_manager_public_ip           = google_compute_address.ops-manager.address
@@ -33,19 +18,33 @@ locals {
     ops_manager_ssh_public_key      = tls_private_key.ops-manager.public_key_openssh
     ops_manager_ssh_private_key     = tls_private_key.ops-manager.private_key_pem
 
-    backend_service_http  = google_compute_backend_service.http-lb.name
-    target_pool_ssh       = google_compute_target_pool.ssh-lb.name
-    target_pool_tcp       = google_compute_target_pool.tcp-lb.name
-    target_pool_websocket = google_compute_target_pool.websocket-lb.name
-    target_pool_pks_api   = google_compute_target_pool.pks-api-lb.name
+    pas_subnet_name    = google_compute_subnetwork.pas.name
+    pas_subnet_cidr    = google_compute_subnetwork.pas.ip_cidr_range
+    pas_subnet_gateway = google_compute_subnetwork.pas.gateway_address
 
-    dns_wildcard_sys    = google_dns_record_set.wildcard-sys.name
-    dns_wildcard_apps   = google_dns_record_set.wildcard-apps.name
-    dns_doppler_sys     = google_dns_record_set.doppler-sys.name
-    dns_loggregator_sys = google_dns_record_set.loggregator-sys.name
-    dns_ssh             = google_dns_record_set.ssh.name
-    dns_tcp             = google_dns_record_set.tcp.name
-    dns_pks_api         = google_dns_record_set.pks-api.name
+    http_backend_service = google_compute_backend_service.http-lb.name
+    ssh_target_pool      = google_compute_target_pool.ssh-lb.name
+    tcp_target_pool      = google_compute_target_pool.tcp-lb.name
+    web_target_pool      = google_compute_target_pool.websocket-lb.name
+
+    sys_dns         = google_dns_record_set.wildcard-sys.name
+    apps_dns        = google_dns_record_set.wildcard-apps.name
+    doppler_dns     = google_dns_record_set.doppler-sys.name
+    loggregator_dns = google_dns_record_set.loggregator-sys.name
+    ssh_dns         = google_dns_record_set.ssh.name
+    tcp_dns         = google_dns_record_set.tcp.name
+
+    pks_subnet_name               = google_compute_subnetwork.pks.name
+    pks_subnet_cidr               = google_compute_subnetwork.pks.ip_cidr_range
+    pks_subnet_gateway            = google_compute_subnetwork.pks.gateway_address
+    pks_master_service_account_id = google_service_account.pks-master-node-service-account.email
+    pks_worker_service_account_id = google_service_account.pks-worker-node-service-account.email
+    pks_api_target_pool_name      = google_compute_target_pool.pks-api-lb.name
+    pks_api_dns                   = google_dns_record_set.pks-api.name
+
+    services_subnet_name    = google_compute_subnetwork.services.name
+    services_subnet_cidr    = google_compute_subnetwork.services.ip_cidr_range
+    services_subnet_gateway = google_compute_subnetwork.services.gateway_address
   }
 }
 
