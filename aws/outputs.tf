252570ce15fa44e9b51298ca18838842d21ee702
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-27 10:20:30 -0500

    Update aws/gcp ops-manager config to match tf output names.

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,5 +1,8 @@
 locals {
   stable_config = {
+    access_key         = var.access_key
+    secret_key         = var.secret_key
+    environment_name   = var.environment_name
     availability_zones = var.availability_zones
     region             = var.region
 
@@ -14,6 +17,7 @@ locals {
     dns_tcp           = aws_route53_record.tcp.name
     dns_pks_api       = aws_route53_record.pks-api.name
 
+    ops_manager_subnet_id                 = aws_subnet.public-subnet[0].id
     ops_manager_public_ip                 = aws_eip.ops-manager.public_ip
     ops_manager_dns                       = aws_route53_record.ops-manager.name
     ops_manager_iam_user_access_key       = aws_iam_access_key.ops-manager.id
@@ -23,9 +27,9 @@ locals {
     ops_manager_ssh_public_key            = tls_private_key.ops-manager.public_key_openssh
     ops_manager_ssh_private_key           = tls_private_key.ops-manager.private_key_pem
     ops_manager_bucket                    = aws_s3_bucket.ops-manager-bucket.bucket
+    ops_manager_security_group_id         = aws_security_group.ops-manager.id
+    ops_manager_security_group_name       = aws_security_group.ops-manager.name
 
-    security_group_ops_manager_id    = aws_security_group.ops-manager.id
-    security_group_ops_manager_name  = aws_security_group.ops-manager.name
     security_group_platform_id       = aws_security_group.platform.id
     security_group_platform_name     = aws_security_group.platform.name
     security_group_nat_id            = aws_security_group.nat.id
