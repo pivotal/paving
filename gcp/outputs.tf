0b97124f694e08adecf5782232fa535cd54edeff
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-27 13:13:37 -0500

    Update gcp sys/apps dns to remove wildcard from output.

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -41,8 +41,8 @@ locals {
     tcp_target_pool_name      = google_compute_target_pool.tcp-lb.name
     web_target_pool_name      = google_compute_target_pool.websocket-lb.name
 
-    sys_dns         = replace(google_dns_record_set.wildcard-sys.name, "/\\.$/", "")
-    apps_dns        = replace(google_dns_record_set.wildcard-apps.name, "/\\.$/", "")
+    sys_dns_domain  = replace(replace(google_dns_record_set.wildcard-sys.name, "/\\.$/", ""), "*.", "")
+    apps_dns_domain = replace(replace(google_dns_record_set.wildcard-apps.name, "/\\.$/", ""), "*.", "")
     doppler_dns     = replace(google_dns_record_set.doppler-sys.name, "/\\.$/", "")
     loggregator_dns = replace(google_dns_record_set.loggregator-sys.name, "/\\.$/", "")
     ssh_dns         = replace(google_dns_record_set.ssh.name, "/\\.$/", "")
@@ -55,7 +55,7 @@ locals {
     pks_master_service_account_id = google_service_account.pks-master-node-service-account.email
     pks_worker_service_account_id = google_service_account.pks-worker-node-service-account.email
     pks_api_target_pool_name      = google_compute_target_pool.pks-api-lb.name
-    pks_api_dns                   = google_dns_record_set.pks-api.name
+    pks_api_dns_domain            = replace(replace(google_dns_record_set.pks-api.name, "/\\.$/", ""), "*.", "")
 
     services_subnet_name               = google_compute_subnetwork.services.name
     services_subnet_cidr               = google_compute_subnetwork.services.ip_cidr_range
