b7aa38cd8c717fc9237a72ade86128dd3ef95e92
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-27 17:28:54 -0500

    Add node to service account id outputs for gcp.

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -48,14 +48,14 @@ locals {
     ssh_dns         = replace(google_dns_record_set.ssh.name, "/\\.$/", "")
     tcp_dns         = replace(google_dns_record_set.tcp.name, "/\\.$/", "")
 
-    pks_subnet_name               = google_compute_subnetwork.pks.name
-    pks_subnet_cidr               = google_compute_subnetwork.pks.ip_cidr_range
-    pks_subnet_gateway            = google_compute_subnetwork.pks.gateway_address
-    pks_subnet_reserved_ip_ranges = "${cidrhost(google_compute_subnetwork.pks.ip_cidr_range, 1)}-${cidrhost(google_compute_subnetwork.pks.ip_cidr_range, 9)}"
-    pks_master_service_account_id = google_service_account.pks-master-node-service-account.email
-    pks_worker_service_account_id = google_service_account.pks-worker-node-service-account.email
-    pks_api_target_pool_name      = google_compute_target_pool.pks-api-lb.name
-    pks_api_dns_domain            = replace(replace(google_dns_record_set.pks-api.name, "/\\.$/", ""), "*.", "")
+    pks_subnet_name                    = google_compute_subnetwork.pks.name
+    pks_subnet_cidr                    = google_compute_subnetwork.pks.ip_cidr_range
+    pks_subnet_gateway                 = google_compute_subnetwork.pks.gateway_address
+    pks_subnet_reserved_ip_ranges      = "${cidrhost(google_compute_subnetwork.pks.ip_cidr_range, 1)}-${cidrhost(google_compute_subnetwork.pks.ip_cidr_range, 9)}"
+    pks_master_node_service_account_id = google_service_account.pks-master-node-service-account.email
+    pks_worker_node_service_account_id = google_service_account.pks-worker-node-service-account.email
+    pks_api_target_pool_name           = google_compute_target_pool.pks-api-lb.name
+    pks_api_dns_domain                 = replace(replace(google_dns_record_set.pks-api.name, "/\\.$/", ""), "*.", "")
 
     services_subnet_name               = google_compute_subnetwork.services.name
     services_subnet_cidr               = google_compute_subnetwork.services.ip_cidr_range
