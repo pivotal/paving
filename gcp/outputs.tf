553d26dc81e5b8c23156665be26f2812aa5c6380
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-27 09:22:19 -0500

    Compute gcp reserved ip ranges in outputs.

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -9,9 +9,10 @@ locals {
 
     hosted_zone_name_servers = data.google_dns_managed_zone.hosted-zone.name_servers
 
-    management_subnet_name    = google_compute_subnetwork.management.name
-    management_subnet_cidr    = google_compute_subnetwork.management.ip_cidr_range
-    management_subnet_gateway = google_compute_subnetwork.management.gateway_address
+    management_subnet_name               = google_compute_subnetwork.management.name
+    management_subnet_cidr               = google_compute_subnetwork.management.ip_cidr_range
+    management_subnet_gateway            = google_compute_subnetwork.management.gateway_address
+    management_subnet_reserved_ip_ranges = "${cidrhost(google_compute_subnetwork.management.ip_cidr_range, 1)}-${cidrhost(google_compute_subnetwork.management.ip_cidr_range, 9)}"
 
     ops_manager_bucket              = google_storage_bucket.ops-manager.name
     ops_manager_dns                 = replace(google_dns_record_set.ops-manager.name, "/\\.$/", "")
@@ -21,9 +22,12 @@ locals {
     ops_manager_ssh_private_key     = tls_private_key.ops-manager.private_key_pem
     ops_manager_tags                = "${var.environment_name}-ops-manager"
 
-    pas_subnet_name    = google_compute_subnetwork.pas.name
-    pas_subnet_cidr    = google_compute_subnetwork.pas.ip_cidr_range
-    pas_subnet_gateway = google_compute_subnetwork.pas.gateway_address
+    platform_vms_tag = "${var.environment_name}-vms"
+
+    pas_subnet_name               = google_compute_subnetwork.pas.name
+    pas_subnet_cidr               = google_compute_subnetwork.pas.ip_cidr_range
+    pas_subnet_gateway            = google_compute_subnetwork.pas.gateway_address
+    pas_subnet_reserved_ip_ranges = "${cidrhost(google_compute_subnetwork.pas.ip_cidr_range, 1)}-${cidrhost(google_compute_subnetwork.pas.ip_cidr_range, 9)}"
 
     buildpacks_bucket_name = google_storage_bucket.buildpacks.name
     droplets_bucket_name   = google_storage_bucket.droplets.name
@@ -46,14 +50,16 @@ locals {
     pks_subnet_name               = google_compute_subnetwork.pks.name
     pks_subnet_cidr               = google_compute_subnetwork.pks.ip_cidr_range
     pks_subnet_gateway            = google_compute_subnetwork.pks.gateway_address
+    pks_subnet_reserved_ip_ranges = "${cidrhost(google_compute_subnetwork.pks.ip_cidr_range, 1)}-${cidrhost(google_compute_subnetwork.pks.ip_cidr_range, 9)}"
     pks_master_service_account_id = google_service_account.pks-master-node-service-account.email
     pks_worker_service_account_id = google_service_account.pks-worker-node-service-account.email
     pks_api_target_pool_name      = google_compute_target_pool.pks-api-lb.name
     pks_api_dns                   = google_dns_record_set.pks-api.name
 
-    services_subnet_name    = google_compute_subnetwork.services.name
-    services_subnet_cidr    = google_compute_subnetwork.services.ip_cidr_range
-    services_subnet_gateway = google_compute_subnetwork.services.gateway_address
+    services_subnet_name               = google_compute_subnetwork.services.name
+    services_subnet_cidr               = google_compute_subnetwork.services.ip_cidr_range
+    services_subnet_gateway            = google_compute_subnetwork.services.gateway_address
+    services_subnet_reserved_ip_ranges = "${cidrhost(google_compute_subnetwork.services.ip_cidr_range, 1)}-${cidrhost(google_compute_subnetwork.services.ip_cidr_range, 9)}"
   }
 }
 
