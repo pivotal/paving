d3ab4a11afc16dd12790940b7f8d0d40deba1ec0
Genevieve L'Esperance <glesperance@pivotal.io>
2020-01-27 10:51:00 -0500

    Decode the ops manager key in outputs.

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,9 +1,10 @@
 locals {
   stable_config = {
-    environment_name   = var.environment_name
-    project            = var.project
-    region             = var.region
-    availability_zones = var.availability_zones
+    environment_name    = var.environment_name
+    service_account_key = var.service_account_key
+    project             = var.project
+    region              = var.region
+    availability_zones  = var.availability_zones
 
     network_name = google_compute_network.network.name
 
@@ -17,7 +18,7 @@ locals {
     ops_manager_bucket              = google_storage_bucket.ops-manager.name
     ops_manager_dns                 = replace(google_dns_record_set.ops-manager.name, "/\\.$/", "")
     ops_manager_public_ip           = google_compute_address.ops-manager.address
-    ops_manager_service_account_key = google_service_account_key.ops-manager.private_key
+    ops_manager_service_account_key = base64decode(google_service_account_key.ops-manager.private_key)
     ops_manager_ssh_public_key      = tls_private_key.ops-manager.public_key_openssh
     ops_manager_ssh_private_key     = tls_private_key.ops-manager.private_key_pem
     ops_manager_tags                = "${var.environment_name}-ops-manager"
