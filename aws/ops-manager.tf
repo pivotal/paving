1ad99f8224d9e7662f2a2474ed437e907c2f4d56
Toshiaki Maki <tmaki@pivotal.io>
2020-06-21 19:13:44 +0900

    Add S3 permissions to PAS buckets

--- aws/ops-manager.tf
+++ aws/ops-manager.tf
@@ -104,7 +104,15 @@ data "aws_iam_policy_document" "ops-manager" {
     actions = ["s3:*"]
     resources = [
       aws_s3_bucket.ops-manager-bucket.arn,
-      "${aws_s3_bucket.ops-manager-bucket.arn}/*"
+      "${aws_s3_bucket.ops-manager-bucket.arn}/*",
+      aws_s3_bucket.buildpacks-bucket.arn,
+      "${aws_s3_bucket.buildpacks-bucket.arn}/*",
+      aws_s3_bucket.packages-bucket.arn,
+      "${aws_s3_bucket.packages-bucket.arn}/*" ,
+      aws_s3_bucket.resources-bucket.arn,
+      "${aws_s3_bucket.resources-bucket.arn}/*" ,
+      aws_s3_bucket.droplets-bucket.arn,
+      "${aws_s3_bucket.droplets-bucket.arn}/*"       
     ]
   }
 
