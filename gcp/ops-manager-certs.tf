ff21c1c820e4c745fbc103a4325fc12acd3f13c7
Kira Boyle <kirab@vmware.com>
2020-10-01 07:27:59 -0600

    Create ssl certificate before destroying it so certs can be rotated
    Signed-off-by: JT Archie <jarchie@vmware.com>

--- gcp/ops-manager-certs.tf
+++ gcp/ops-manager-certs.tf
@@ -2,4 +2,8 @@ resource "google_compute_ssl_certificate" "certificate" {
   name_prefix = "${var.environment_name}-cert"
   certificate = var.ssl_certificate
   private_key = var.ssl_private_key
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
