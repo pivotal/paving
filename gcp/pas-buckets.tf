3c4fb0007003e478e400f5b466a1c29475fb396d
Ramkumar Vengadakrishnan <rvengadakrishnan@pivotal.io>
2021-11-15 16:14:59 -0600

    Added location to gcp bucket resource
    Terraform 0.14.0 needs location as a required
    parameter to the google_storage_bucket resource
    
    Authored-by: Ramkumar Vengadakrishnan <rvengadakrishnan@pivotal.io>
    Authored-by: Jhonathan Aristizabal <jhonathana@vmware.com>
    
    [#180184728] Upgrade Terraform and Paving Repo Terraform templates

--- gcp/pas-buckets.tf
+++ gcp/pas-buckets.tf
@@ -1,24 +1,29 @@
 resource "google_storage_bucket" "buildpacks" {
   name          = "${var.project}-${var.environment_name}-buildpacks-${random_integer.bucket_suffix.result}"
   force_destroy = true
+  location = var.location
 }
 
 resource "google_storage_bucket" "droplets" {
   name          = "${var.project}-${var.environment_name}-droplets-${random_integer.bucket_suffix.result}"
   force_destroy = true
+  location = var.location
 }
 
 resource "google_storage_bucket" "packages" {
   name          = "${var.project}-${var.environment_name}-packages-${random_integer.bucket_suffix.result}"
   force_destroy = true
+  location = var.location
 }
 
 resource "google_storage_bucket" "resources" {
   name          = "${var.project}-${var.environment_name}-resources-${random_integer.bucket_suffix.result}"
   force_destroy = true
+  location = var.location
 }
 
 resource "google_storage_bucket" "backup" {
   name          = "${var.project}-${var.environment_name}-backup-${random_integer.bucket_suffix.result}"
   force_destroy = true
+  location = var.location
 }
