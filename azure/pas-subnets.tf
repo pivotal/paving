8fb40516fd8d11cf00aee3817e98c03a09354896
Jhonathan Aristizabal <jhonathana@vmware.com>
2021-11-17 09:32:09 -0500

    Azure: Fixed warnings on azurerm_subnet - Removed warnings on azurerm_subnets.address_prefix
    AWS: Upgraded provider to 3.65.0

--- azure/pas-subnets.tf
+++ azure/pas-subnets.tf
@@ -3,7 +3,7 @@ resource "azurerm_subnet" "pas" {
 
   resource_group_name  = azurerm_resource_group.platform.name
   virtual_network_name = azurerm_virtual_network.platform.name
-  address_prefix    = var.pas_subnet_cidr
+  address_prefixes    = [ var.pas_subnet_cidr ]
 
 }
 
--- gcp/versions.tf
+++ gcp/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "~>4.1.0"
+      version = "~> 4.1"
     }
     random = {
       source = "hashicorp/random"
