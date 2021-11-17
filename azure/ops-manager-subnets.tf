476ce1a50c5e7b4e9ab96a22b74abe9563581ca5
Jhonathan Aristizabal <jhonathana@vmware.com>
2021-11-17 09:32:09 -0500

    Azure: Fixed warnings on azurerm_subnet - Removed warnings on azurerm_subnets.address_prefix
    AWS: Upgraded provider to 3.65.0

--- azure/ops-manager-subnets.tf
+++ azure/ops-manager-subnets.tf
@@ -3,7 +3,7 @@ resource "azurerm_subnet" "management" {
 
   resource_group_name  = azurerm_resource_group.platform.name
   virtual_network_name = azurerm_virtual_network.platform.name
-  address_prefix     = var.management_subnet_cidr
+  address_prefixes     = [ var.management_subnet_cidr ]
 
 }
 
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "services" {
 
   resource_group_name  = azurerm_resource_group.platform.name
   virtual_network_name = azurerm_virtual_network.platform.name
-  address_prefix     = var.services_subnet_cidr
+  address_prefixes     = [ var.services_subnet_cidr ]
 
 }
 
--- azure/pks-subnets.tf
+++ azure/pks-subnets.tf
@@ -3,7 +3,7 @@ resource "azurerm_subnet" "pks" {
 
   resource_group_name  = azurerm_resource_group.platform.name
   virtual_network_name = azurerm_virtual_network.platform.name
-  address_prefix     = var.pks_subnet_cidr
+  address_prefixes     = [ var.pks_subnet_cidr ]
 
 }
 
