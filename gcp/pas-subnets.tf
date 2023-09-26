ed68d65357e6dbb0047ba614ff53cf36fc756a6f
claire tinati <mrt59@cornell.edu>
2023-09-26 14:04:00 -0400

    fix: add vpc flow sampling to comply with new org policy

--- gcp/pas-subnets.tf
+++ gcp/pas-subnets.tf
@@ -3,4 +3,10 @@ resource "google_compute_subnetwork" "pas" {
   ip_cidr_range = local.pas_subnet_cidr
   network       = google_compute_network.network.self_link
   region        = var.region
+
+  log_config {
+    aggregation_interval = "INTERVAL_15_MIN"
+    flow_sampling = "1.0"
+    metadata = "INCLUDE_ALL_METADATA"
+  }
 }
